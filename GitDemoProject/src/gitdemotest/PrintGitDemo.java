4d266297624d4987d89f29f73fe2eec0a4f63a6b
Anupriyanka <anupriyanka23@gmail.com>
2020-08-13 13:51:49 +0530

    "refs Git demo change"

--- GitDemoProject/src/gitdemotest/PrintGitDemo.java
+++ GitDemoProject/src/gitdemotest/PrintGitDemo.java
@@ -3,7 +3,7 @@ package gitdemotest;
 public class PrintGitDemo {
 
 	public static void main(String[] args) {
-	      System.out.println("Hello, world!");
+	      System.out.println("Welcome to the GIT HUB project");
 	   }
 
 }
